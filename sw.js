a0d9438fbd08ea7e56a827621490b00c031b0a7e
adkow <a.kowalewski.student@gmail.com>
2021-11-01 19:09:31 +0100

    first commit

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('').then((cache) => cache.addAll([
+        '/index.html',
+        '/style1.css',
+        '/style2.css',
+        '/style3.css',
+        '/index.js',
+        '/icon/example-icon.png'
+      ])),
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
